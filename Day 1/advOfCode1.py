009c877ef76fb89b8e700b8e035f23975a9b0751
konrathj <konrathj@student.ethz.ch>
2020-12-03 15:58:55 +0100

    .

--- Day 1/advOfCode1.py
+++
@@ -1,9 +0,0 @@
-lines = open("advOfCode1.txt").readlines();
-vals = [];
-for line in lines:
-	vals.append(int(line))
-
-for i in range(0,len(vals)):
-	for j in range(i,len(vals)):
-		if(vals[i]+vals[j] == 2020):
-			print(vals[i]*vals[j])
--- advOfCodeTemplate.py
+++
@@ -1,6 +0,0 @@
-DAY = #DAY NUMBER HERE
-lines = open(f"advOfCode{DAY}").readlines()
-
-for line in lines:
-	#do Parsing stuff
-	continue
