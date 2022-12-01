1bd11ea771b55b0608b63a28aa1d7f95955d6d5c
nighmared <jonas.konrath@icloud.com>
2022-12-01 19:52:24 +0100

    2022 day1

---
+++ 2022/day1/solve.py
@@ -0,0 +1,26 @@
+with open("input.txt","r") as f:
+	lines = f.readlines()
+
+elves = [0]
+for line in lines:
+	if line == "\n":
+		elves.append(0)
+	else:
+		elves[-1] += int(line)
+
+
+elves.sort()
+
+
+print("================== PART 1 =================")
+
+print("Elve carrying the most calories is carrying this many calories:")
+print(elves[-1])
+
+
+print("================== PART 2 =================")
+print("Amount of calories carried by the 3 elves that carry \nthe most, second most and third most:")
+print(elves[-1]+elves[-2]+elves[-3])
+
+
+
