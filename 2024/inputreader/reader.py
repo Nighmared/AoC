33431df5d0f473778a8d55a4558f674238f5f915
nighmared <jonas.konrath@icloud.com>
2024-12-08 16:43:48 +0100

    now with input downloading hehe

--- 2024/inputreader/reader.py
+++ 2024/inputreader/reader.py
@@ -5,11 +5,8 @@ class InputReader:
 
     def __init__(self, fpath) -> None:
         self.fname = fpath
-        try:
-            with open(fpath, "r", encoding="utf-8") as f:
-                self.content = f.read()
-                self.lines = self.content.split("\n")
-                if self.lines[-1].strip() == "":
-                    self.lines.pop()
-        except FileNotFoundError:
-            print("No such file", fpath)
+        with open(fpath, "r", encoding="utf-8") as f:
+            self.content = f.read()
+            self.lines = self.content.split("\n")
+            if self.lines[-1].strip() == "":
+                self.lines.pop()
--- 2024/main.py
+++ 2024/main.py
@@ -1,8 +1,9 @@
-import getopt
 from datetime import datetime
 from sys import argv
+from sys import exit as sysexit
 
 import days
+import requests
 from inputreader import InputReader
 
 
@@ -11,7 +12,26 @@ def mk_input_path(day_nr: int, sample: bool = False) -> str:
     return f"days/day{str(day_nr).zfill(2)}/{name}"
 
 
+def download_and_store(day_nr: int) -> bool:
+    with open("session.secret", "r", encoding="utf-8") as secret:
+        session = secret.read().strip()
+
+    resp = requests.get(
+        f"https://adventofcode.com/2024/day/{day_nr}/input",
+        cookies={"session": session},
+        timeout=3,
+    )
+    if resp.status_code != 200:
+        return False
+    path = mk_input_path(day_nr=day_nr)
+    with open(path, "w", encoding="utf-8") as file:
+        file.write(resp.content.decode())
+    return True
+
+
 def main() -> None:
+    # TODO good command parsing
+    # with p1/p2 etc etc
     day = -1
     if (
         (len(argv) == 1 or (len(argv) == 2 and not argv[-1].isnumeric()))
@@ -28,13 +48,32 @@ def main() -> None:
         sample = True
         print("[*] Using sample input because last argument is a flag for it")
     solver_cls = days.days[day]
-    solver = solver_cls(InputReader(mk_input_path(day, sample)))
+    try:
+        path = mk_input_path(day, sample)
+        in_reader = InputReader(path)
+    except FileNotFoundError:
+        if sample:
+            print("[!] Sample file not found, please store it at", path)
+            sysexit(1)
+        else:
+            print("[?] Input file not found, trying to download")
+            res = download_and_store(day)
+            if not res:
+                print("[!] Downloading failed. Are you too early? Exiting!")
+                sysexit(1)
+            else:
+                print(f"[+] Downloaded input for day {day} and stored at {path}")
+                in_reader = InputReader(path)
+
+    solver = solver_cls(in_reader)
+    res1 = solver.part1()
     if sample:
         print("[SAMPLE INPUT]", end="")
-    print("[Part 1]", solver.part1())
+    print("[Part 1]", res1)
+    res2 = solver.part2()
     if sample:
         print("[SAMPLE INPUT]", end="")
-    print("[Part 2]", solver.part2())
+    print("[Part 2]", res2)
 
 
 if __name__ == "__main__":
